f50bc72ae84751be17c58a6d9ec21c2461170d03
Mag1cc <matisoch04@gmail.com>
2019-11-30 19:59:42 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const { Client, Collection } = require("discord.js");
 const { config } = require("dotenv");
 const fs = require("fs");
-const ft = require("fortnite");
 const client = new Client({
     disableEveryone: true
 });
