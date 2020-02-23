0330fa6c131af9b843c11138af22a2c4cb80c4c5
Mag1cc <matisoch04@gmail.com>
2020-02-23 11:04:41 +0000

    urban.js WORKING DO NOT TOUCH

--- commands/fun/urban.js
+++ commands/fun/urban.js
@@ -1,32 +1,28 @@
-const urban = require('urban-dictionary');
 const { RichEmbed } = require("discord.js");
+const ud = require("relevant-urban");
+
 module.exports = {
-   name: "urban",
-   aliases: ["urban", "Urban"],
-   category: "fun",
-   description: "Gets definition from urban dictionary",
-   usage: "[args input]",
-   run: async(client, message, args, tools) => {
-      if (!args[0]) return message.channel.send(`***please write somthing***`);
+    name: "urban",
+    aliases: ["define"],
+    category: "fun",
+    description: "finds an urban dictionary definition",
+    run: async (client, message, args) => {
+        if (message.channel.nsfw) return message.channel.send("You can only send messages in a non-NSFW marked channel.")
+    let worder = args[0];
+        if(!worder) return message.channel.send("Specify a word")
+    let defin = await ud(args.join(' ')).catch(e => {
+    message.channel.send("Word not found")
+    return;
+});
 
-      let res = await urban(args.join('')).catch(e => { 
-         return message.channel.send(`***sorry word not found!***`)
-   });
+let embed = new RichEmbed()
+.setTitle(defin.word)
+.setURL(defin.urbanURL)
+.setDescription(defin.definition)
+.addField("Example", defin.example)
+.addField("Author", defin.author)
+.setColor("RANDOM")
+message.channel.send(embed)
 
-   const embed = new RichEmbed()
-       .setColor('RANDOM')
-       .setTitle(res.word)
-       .setURL(res.urbanURL)
-       .setDescription(`**Definition:**\n*${res.definition}*\n\n**Example:**\n*${res.example}*`)
-       .addField('Author', res.author, true)
-       .addField('Rating', `**\`Upvotes: &{res.thumbsUp} | Downvotes: &{res.thumbsDown}\`**`)
-    
-   if  (res.tags.length > 0 && res.tags.join(' ').length < 1024) {
-       embed.addField('Tags', res.tags.join(', '), true)
-
-   }
-
-   message.channel.send(embed);
-
-  }
+    }
 }
