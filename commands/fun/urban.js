b03bc0128f98db7f6e76638e542515b1afdd76db
Aidan McWall <32238878+Aidarooney@users.noreply.github.com>
2020-02-23 10:47:49 +0000

    Update urban.js

--- commands/fun/urban.js
+++ commands/fun/urban.js
@@ -1,34 +1,33 @@
 const urban = require('urban-dictionary'),
-        Discord = require('discord.js');
+const { RichEmbed } = require("discord.js");
 
-    module.exports = {
-        name: "urban",
-        aliases: ["urban", "Urban"],
-        category: "fun",
-        description: "Gets definition from urban dictionary",
-        usage: "[args input]",
-        run: async(client, message, args, tools) => {
-            if (!args[0]) return message.channel.send(`***please write somthing***`);
+   module.exports = {
+       name: "urban",
+       aliases: ["urban", "Urban"],
+       category: "fun",
+       description: "Gets definition from urban dictionary",
+       usage: "[args input]",
+       run: async(client, message, args, tools) => {
+           if (!args[0]) return message.channel.send(`***please write somthing***`);
 
-    let res = await urban(args.join('')).catch(e => { 
-        return message.channel.send(`***sorry word not found!***`)
-    });
+   let res = await urban(args.join('')).catch(e => { 
+       return message.channel.send(`***sorry word not found!***`)
+   });
 
-    const embed = new Discord.MessageEmbed()
-        .setColor('RANDOM')
-        .setTitle(res.word)
-        .setURL(res.urbanURL)
-        .setDescription(`**Definition:**\n*${res.definition}*\n\n**Example:**\n*${res.example}*`)
-        .addField('Author', res.author, true)
-        .addField('Rating', `**\`Upvotes: &{res.thumbsUp} | Downvotes: &{res.thumbsDown}\`**`)
+   const embed = new Discord.MessageEmbed()
+       .setColor('RANDOM')
+       .setTitle(res.word)
+       .setURL(res.urbanURL)
+       .setDescription(`**Definition:**\n*${res.definition}*\n\n**Example:**\n*${res.example}*`)
+       .addField('Author', res.author, true)
+       .addField('Rating', `**\`Upvotes: &{res.thumbsUp} | Downvotes: &{res.thumbsDown}\`**`)
     
-    if  (res.tags.length > 0 && res.tags.join(' ').length < 1024) {
-        embed.addField('Tags', res.tags.join(', '), true)
+   if  (res.tags.length > 0 && res.tags.join(' ').length < 1024) {
+       embed.addField('Tags', res.tags.join(', '), true)
 
-    }
+   }
 
-    message.channel.send(embed);
+   message.channel.send(embed);
 
+  }
 }
-
-    }
