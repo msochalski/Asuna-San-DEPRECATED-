0bf07cd6071cb9efad7cc9b602602e5d08ce51b8
Mag1cc <matisoch04@gmail.com>
2020-03-01 15:06:51 +0000

    Create car.js

---
+++ commands/fun/car.js
@@ -0,0 +1,23 @@
+Learn more or give us feedback
+const { RichEmbed } = require("discord.js");
+const randomPuppy = require("random-puppy");
+
+module.exports = {
+    name: "car",
+    aliases: "cars",
+    category: "fun",
+    description: "sends a random image from subreddit",
+    run: async (client, message, args) => {
+        const subReddits = ["RoastMyCar"];
+        const random = subReddits[Math.floor(Math.random() * subReddits.length)];
+
+        const img = await randomPuppy(random);
+        const embed = new RichEmbed()
+            .setColor("RANDOM")
+            .setImage(img)
+            .setTitle(`From /r/${random}`)
+            .setURL(`https://reddit.com/r/${random}`);
+
+            message.channel.send(embed);
+    }
+}
