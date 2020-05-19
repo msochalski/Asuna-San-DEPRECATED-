dfe1313708379f6c715542454dc7d7e34793451f
Mag1cc <matisoch04@gmail.com>
2020-05-19 21:34:56 +0100

    Update hug.js

--- commands/reactions/hug.js
+++ commands/reactions/hug.js
@@ -9,7 +9,7 @@ module.exports = {
         if (!message.mentions.users.first()) return message.reply("You need to mention someone to hug them");
         if (message.mentions.users.first().id === message.author.id) return message.channel.send('I can\'t let you do that, that is lonely:facepalm:');
         const { body } = await superagent
-        .get("https://nekos.life/api/v2/hug");
+        .get("https://nekos.life/api/v2/img/hug");
         
         const embed = new Discord.RichEmbed()
         .setColor("#ff9900")
