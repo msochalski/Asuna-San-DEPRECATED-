691c10e0dcf417e05e5e650ad3f19982e795bddf
Mag1cc <msochalski226@c2ken.net>
2020-05-19 21:26:04 +0100

    Add files via upload

---
+++ commands/reactions/hug.js
@@ -0,0 +1,24 @@
+const { RichEmbed } = require("discord.js");
+const fetch = require('node-fetch');
+ 
+module.exports = {
+    name: "hug",
+    aliases: ["hugs"],
+    category: "reaction",
+    description: "sends gifs of hugs",
+    run: async (client, message, args) => {
+            try {
+              const author = message.author.username;
+              const user = member ? member.user.username : message.language.get('THEMSELVES');
+              const data = await (await fetch('https://nekos.life/api/v2/img/hug')).json();
+              if (!(data || data.url)) return message.sendError('NO_DATA');
+              message.genEmbed()
+                  .setEmoteTitle(author, user, 'HUGGING', true)
+                  .setProvidedBy('nekos.life')
+                  .setImage(data.url)
+                  .send();
+            } catch (error) {
+              return message.sendError('REQUEST_FAILED');
+      }
+   }
+};
